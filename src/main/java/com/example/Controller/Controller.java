6ba9d5f8dda3cae382a1058307a5f9a594af1c62
patidar596 <69580195+patidar596@users.noreply.github.com>
2025-10-19 12:09:40 +0530

    Revert "Created new login page using thymleaf"

--- src/main/java/com/example/Controller/Controller.java
+++ src/main/java/com/example/Controller/Controller.java
@@ -1,21 +1,11 @@
 package com.example.Controller;
 
 import com.example.Model.TestModel;
-import com.example.Model.User;
-import com.example.Service.UserService;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@org.springframework.stereotype.Controller
+@RestController
 public class Controller {
-
-    @Autowired
-    private UserService userService;
 
     @GetMapping("/test")
     public String test(){
@@ -27,32 +17,5 @@ public class Controller {
         TestModel testModel = new TestModel();
         testModel.setTest("1234");
         return testModel;
-    }
-
-    @GetMapping("/login")
-    public String showLoginPage(Model model){
-        model.addAttribute("user", new User());
-        return "login";
-    }
-
-    @PostMapping("/login")
-    public String processLogin(@ModelAttribute("user") User user, HttpSession session, Model model) {
-        if (userService.authenticate(user)) {
-            session.setAttribute("username", user.getUsername());
-            return "redirect:/home";
-        } else {
-            model.addAttribute("error", "Invalid username or password");
-            return "login";
-        }
-    }
-
-    @GetMapping("/home")
-    public String home(HttpSession session, Model model) {
-        String username = (String) session.getAttribute("username");
-        if (username == null) {
-            return "redirect:/login";
-        }
-        model.addAttribute("username", username);
-        return "home";
     }
 }
